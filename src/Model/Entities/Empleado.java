9082fb5ab20d1f5af1194f9a0a82bf35f4646f4a
Luz Dary Crespo <luzdaricrespo@gmail.com>
2025-10-01 15:44:29 -0500

    feat(empleado): implementar DAO con operaciones CRUD usando JSON
    Se agregó la clase EmpleadoDAO con métodos para registrar, obtener,
    actualizar y eliminar empleados. También se añadió persistencia en
    archivo JSON utilizando Gson y soporte para LocalDate.

--- src/Model/Entities/Empleado.java
+++ src/Model/Entities/Empleado.java
@@ -1,10 +1,115 @@
 
 package Model.Entities;
 
-
 public class Empleado {
    
-    private boolean isAdmin = false;
+    private Usuario usuario;
+
+    private String idEmpleado;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String edad;
+    private String identificacion;
+    private String correo;
+    private String telefono;
+
+    public Empleado(Usuario usuario, String idEmpleado, String primerNombre, String segundoNombre, String primerApellido, String segundoApellido, String edad, String identificacion, String correo, String telefono) {
+        this.usuario = usuario;
+        this.idEmpleado = idEmpleado;
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.edad = edad;
+        this.identificacion = identificacion;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
     
     
 }
